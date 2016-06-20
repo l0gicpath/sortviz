9dd12755999367b746408307469ddd15cfd66be3
Hady Ahmed <hady.fathy@gmail.com>
2016-06-20 12:52:57 +0200

    Complete the implementation of the plugin system
    Added a proper plugin system but still not integrated with the rest of
    the program.

--- lib/sortviz/algorithms.rb
+++ lib/sortviz/algorithms.rb
@@ -1,22 +1,32 @@
 module Sortviz
   class Algorithms
+    class << self
+      def define(algorithm_name, &block)
+        plugins << { display_name: algorithm_name }
+        instance_eval &block
+      end
 
-    def self.define(algorithm_name, &block)
-    end
+      def plugins
+        @plugins ||= []
+      end
 
-    def plugins
-      @plugins ||= []
-    end
+      private
 
-    private
+      def sort(&sort_block)
+        plugins.last[:sort] = sort_block
+      end
 
-    def name(name)
-    end
+      def name(name)
+        plugins.last[:name] = name
+      end
 
-    def author(name)
-    end
+      def author(author)
+        plugins.last[:author] = author
+      end
 
-    def url(link)
+      def url(link)
+        plugins.last[:url] = link
+      end
     end
   end
 end
--- spec/sortviz_algorithms_spec.rb
+++ spec/sortviz_algorithms_spec.rb
@@ -10,4 +10,17 @@ describe Sortviz::Algorithms do
     subject.define('Fake Sort') {}
     expect(subject.plugins.length).to eq(1)
   end
+
+  it 'should evaluate the block given to #define in the context of Sortviz::Algorithms' do
+    subject.define 'Empire Sort' do
+      author 'Luke Skywalker'
+      url 'http://www.starwars.com'
+      name :'empire-sort'
+      sort { puts 'the force is weak with this one' }
+    end
+    expect(subject.plugins.last[:author]).to eq('Luke Skywalker')
+    expect(subject.plugins.last[:url]).to eq('http://www.starwars.com')
+    expect(subject.plugins.last[:name]).to eq(:'empire-sort')
+    expect(subject.plugins.last[:sort]).to respond_to(:call)
+  end
 end
