ab7fae9ff27b8b7e92cea1495ece99eb74029064
Hady Ahmed <hady.fathy@gmail.com>
2016-06-20 05:29:33 +0200

    Update documentation and explicitly state gem versions in gemspec

--- lib/sortviz/plugins.rb
+++ lib/sortviz/plugins.rb
@@ -3,15 +3,16 @@ module Sortviz
   ALGORITHMS = {}
 
   # Defines a new algorithm for sorting by adding some meta data to
-  # +Sortviz::ALGORITHMS+ and turning the sorting algorithm method into a
-  # Module function of +Sortviz+.
+  # <tt>Sortviz::ALGORITHMS</tt> and turning the sorting algorithm method into a
+  # Module function of <tt>Sortviz</tt>.
   #
   # It's a bad design, should be fixed soon with a proper plugin system as this
   # way pollutes the Sortviz namespace.
+  #
   # Params:
-  # +name+:: +String+ Name of the sorting algorithm for display purposes
-  # +func_name+:: +Symbol+ A symbol representation of the actual method that does
-  # the actual sorting. It's important that both match or else +Sortviz::Visualizer+
+  # +name+:: <tt>String</tt> Name of the sorting algorithm for display purposes
+  # +func_name+:: <tt>Symbol</tt> A symbol representation of the actual method that does
+  # the actual sorting. It's important that both match or else <tt>Sortviz::Visualizer</tt>
   # will not be able to find it and call it.
   def define_algorithm(name, func_name)
     ALGORITHMS[func_name] = name
